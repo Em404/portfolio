52adf0653b298ff342ff27cd7fdad55330ad7fa2
Em404 <emanuele.macchiarulo.1998@gmail.com>
2024-05-29 22:01:26 +0200

    start projects component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,9 @@
 import React, { Fragment }  from 'react'
 import { delay, motion } from "framer-motion"
+// import portfolio from ""
+import bsample from "../img/B-Sample-Angular.png"
+import nutrizionista from "../img/Nutrizionista-React.png"
+import neonweather from "../img/Neon-Weather-React.png"
 
 export const Projects = () => {
 
@@ -7,35 +11,35 @@ export const Projects = () => {
     {
       id: 0,
       name: 'Il mio prtfolio',
-      thumb: '',
+      thumb: neonweather,
       description: '',
       url: ''
     },
     {
       id: 1,
       name: 'B-Sample',
-      thumb: '',
+      thumb: bsample,
       description: '',
       url: ''
     },
     {
       id: 2,
       name: 'Studio Professionale di Nutrizione | Gaspa - Micheli',
-      thumb: '',
+      thumb: nutrizionista,
       description: '',
       url: ''
     },
     {
       id: 3,
       name: 'Neon Weather',
-      thumb: '',
+      thumb: neonweather,
       description: '',
       url: ''
     },
     {
       id: 4,
       name: 'Avenue 30',
-      thumb: '',
+      thumb: neonweather,
       description: '',
       url: ''
     },
@@ -75,8 +79,25 @@ export const Projects = () => {
             }}
             transition={{duration: 1, ease: "easeOut", delay: 0.2}}
             className='text-xl md:text-2xl lg:text-3xl font-bold text-center lg:text-left text-purple-900 dark:text-neutral-100 mb-8 md:mb-16'>Projects</motion.h2>
-            <div className='grid grid-cols-2 md:grid-cols-4 gap-8 lg:gap-8'>
-
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-8'>
+            {
+                projects.map((project, index) => {
+                  return (
+                    <motion.div
+                      variants={fadeInUpAnimation}
+                      initial='initial'
+                      whileInView='animate'
+                      viewport={{
+                        once: true
+                      }}
+                      custom={index}
+                      key={index} className='flex flex-col items-center py-4'>
+                      <img src={project.thumb} alt={project.name} className="w-48 h-32"/>
+                      <h3 className='mt-2 font-semibold md:text-lg'>{project.name}</h3>
+                    </motion.div>
+                  )
+                })
+              }
             </div>
       </div>
     </Fragment>
